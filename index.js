77040d894b016abe3df2ec8185f8aeeadbcbb6e5
Anne Koice M <koice.media@gmail.comgit config --global user.name kicciless>
2024-09-04 16:33:29 +0100

    updated final code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const cors=require('express')
+const cors=require('cors')
 const port=process.env.PORT
 const characters=require('./harrypotter.json')
 
